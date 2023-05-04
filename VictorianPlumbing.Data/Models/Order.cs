82cdaf9d1f22b6f02ef62415479f9ec9cf9cf44c
IEmmaB <131818409+IEmmaB@users.noreply.github.com>
2023-05-04 12:13:59 +0100

    Data Project

---
+++ VictorianPlumbing.Data/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VictorianPlumbing.Data.Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderItem> Items { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
---
+++ VictorianPlumbing.Data/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VictorianPlumbing.Data.Models;
+
+namespace VictorianPlumbing.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("CONNECTION STRING HERE");
+        }
+    }
+}
